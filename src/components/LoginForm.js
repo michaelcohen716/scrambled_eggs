1b4fb230dee9c43c63f8781b6b9b7382f761932b
Michael Cohen <michaelcohen716@gmail.com>
2018-03-17 19:08:33 -0400

    fix login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -73,37 +73,41 @@ class LoginForm extends React.Component {
 
   render() {
     return (
-      <View>
-        <Logo />
+      <View style={{flex: 1, flexDirection: 'column'}}>
+        <View style={{flex: 2}}>
+          <Logo />
+        </View>
+        <View style={{flex: 2}}>
+          <Card>
+            <CardSection>
+              <Input
+                label="Email"
+                placeholder="email@gmail.com"
+                onChangeText={this.onEmailChange.bind(this)}
+                value={this.props.email}
+                />
+            </CardSection>
 
-        <Card>
-          <CardSection>
-            <Input
-              label="Email"
-              placeholder="email@gmail.com"
-              onChangeText={this.onEmailChange.bind(this)}
-              value={this.props.email}
-              />
-          </CardSection>
+            <CardSection>
+              <Input
+                secureTextEntry
+                label="Password"
+                placeholder="password"
+                onChangeText={this.onPasswordChange.bind(this)}
+                value={this.props.password}
+                />
+            </CardSection>
 
-          <CardSection>
-            <Input
-              secureTextEntry
-              label="Password"
-              placeholder="password"
-              onChangeText={this.onPasswordChange.bind(this)}
-              value={this.props.password}
-              />
-          </CardSection>
+            <Text style={styles.errorTextStyle}>
+              {this.props.error}
+            </Text>
 
-          <Text style={styles.errorTextStyle}>
-            {this.props.error}
-          </Text>
+            {this.renderAuthButtons()}
 
-          {this.renderAuthButtons()}
+          </Card>
+        </View>
 
-        </Card>
-
+        <View style={{flex: 3}} />
 
       </View>
     );
