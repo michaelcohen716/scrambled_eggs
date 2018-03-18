57ad479feb6a855c3ab86f86cd78fc65d91f82da
Michael Cohen <michaelcohen716@gmail.com>
2018-03-18 00:55:44 -0400

    continue scramble intro

--- src/components/ScrambleIntro.js
+++ src/components/ScrambleIntro.js
@@ -15,12 +15,28 @@ class ScrambleIntro extends React.Component {
   introModal1(){
     const styles = {
         topBarHolder: {
-          flex: 16,
+          flex: 13,
           flexDirection: 'column',
           backgroundColor: 'black',
-          opacity: 0.5,
-          alignItems: 'center',
-          justifyContent: 'center'
+          opacity: 0.3,
+          // alignItems: 'center',
+          // justifyContent: 'center'
+        },
+        topHalf: {
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'flex-end'
+        },
+        bottomHalf: {
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'flex-end',
+          alignItems: 'flex-end'
+        },
+        messageText: {
+          fontSize: 16,
+          fontFamily: 'RobotoCondensed-Regular',
+          color: 'white'
         }
     };
 
@@ -28,11 +44,27 @@ class ScrambleIntro extends React.Component {
       <Modal animationType="slide" transparent={true} visible={this.state.introVisible}>
         <View style={{flex: 1, flexDirection: 'column'}}>
           <View style={{flex: 4}}/>
-          <View style={{flex: 4, backgroundColor: 'black', opacity: 0.9}}/>
+          <View style={{flex: 3, backgroundColor: 'black', opacity: 0.9}}/>
+
           <View style={styles.topBarHolder}>
+            <View style={styles.topHalf}>
+              <View style={{marginLeft: 8, marginBottom: 8}}>
+                <Text style={styles.messageText}>
+                  Use some of these letters
+                </Text>
+              </View>
+            </View>
+
+            <View style={styles.bottomHalf}>
+              <View style={{marginRight: 8, marginBottom: 8}}>
+                <Text style={styles.messageText}>
+                  to define this word
+                </Text>
+              </View>
+            </View>
 
           </View>
-          <View style={{flex: 18, backgroundColor: 'black', opacity: 0.9}} />
+          <View style={{flex: 22, backgroundColor: 'black', opacity: 0.9}} />
 
         </View>
       </Modal>
