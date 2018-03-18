4f59a9a77f3fedc7391cc86b1de565c401c0584d
Michael Cohen <michaelcohen716@gmail.com>
2018-03-17 23:20:15 -0400

    finish ladder intro2

--- src/components/LadderIntro.js
+++ src/components/LadderIntro.js
@@ -9,19 +9,110 @@ class LadderIntro extends React.Component {
       introVisible: true
     };
     this.introModal1 = this.introModal1.bind(this);
+    this.introModal2 = this.introModal2.bind(this);
     this.advance = this.advance.bind(this);
-    
+
   }
 
   advance(){
     if(this.state === 3){
       this.setState({ modalVisible: false});
-      this.props.unpauseTimer();
+      // this.props.unpauseTimer();
     }
     this.setState({ introModal: this.state.introModal + 1});
   }
 
+  introModal3(){
+    const styles = {
+
+    };
+
+    return (
+      <Modal>
+
+      </Modal>
+    );
+  }
+
+  introModal2(){
+    const styles = {
+      topFiller: {
+        flex: 8,
+        backgroundColor: 'black',
+        opacity: 0.9
+      },
+      messageHolder: {
+        flex: 4,
+        flexDirection: 'column',
+        backgroundColor: 'black',
+        opacity: 0.9,
+        alignItems: 'center',
+        justifyContent: 'center'
+      },
+      barHolder: {
+        flex: 10,
+        borderColor: 'yellow',
+        borderWidth: 3,
+        backgroundColor: 'transparent'
+      },
+      advanceHolder: {
+        flex: 3,
+        backgroundColor: 'black',
+        opacity: 0.9,
+        justifyContent: 'center',
+        alignItems: 'flex-end'
+      },
+      smallText: {
+        fontFamily: 'RobotoCondensed-Regular',
+        fontSize: 18,
+        color: 'white',
+        fontWeight: 'bold'
+      },
+      advance: {
+        width: 80,
+        height: 35,
+        borderColor: 'white',
+        borderWidth: 2,
+        borderRadius: 4,
+        backgroundColor: 'black',
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      advanceText: {
+        color: 'white',
+        fontFamily: 'RobotoCondensed-Regular',
+        fontSize: 18,
+      }
+    };
+
+    return (
+      <Modal animationType="slide" transparent={true} visible={this.state.introVisible}>
+        <View style={{flex: 1, flexDirection: 'column'}}>
+          <View style={{flex: 4 }} />
+          <View style={styles.topFiller} />
+          <View style={styles.messageHolder}>
+            <Text style={styles.smallText}>
+              Use
+              <Text style={{fontStyle: 'italic'}}> those </Text>
+              letters to find a word one letter shorter
+            </Text>
+          </View>
+          <View style={styles.barHolder} />
+
+          <View style={styles.advanceHolder}>
+            <TouchableOpacity style={styles.advance} onPress={this.advance}>
+              <Text style={styles.advanceText}>Got it</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={{flex: 15, backgroundColor: 'black', opacity: 0.9}} />
+        </View>
+      </Modal>
+    );
+  }
+
   introModal1(){
+    // 4, 3, 4, 4, 4, 23 = 42
     const styles = {
       topBarHolder: {
         flex: 4,
@@ -112,6 +203,7 @@ class LadderIntro extends React.Component {
     return (
       <View>
         {this.state.introModal === 1 ? this.introModal1() : null}
+        {this.state.introModal === 2 ? this.introModal2() : null}
       </View>
     );
   }
